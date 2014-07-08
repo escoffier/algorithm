7d036c39137fdfbb8a428144ae59983919441d42
robbie <qunfengqiu@gmail.com>
2014-07-08 14:28:13 +0800

    add 5-2

---
+++ tree/5-2/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+template<class T> T max( T a[], int l, int r )
+{
+    std::cout<<"l = "<<l<<"  r = "<<r<<std::endl;
+    if(l == r)
+	{
+	    return a[l];
+	}
+	int mid = (l + r)/2;
+	
+	int lmax = max(a, l, mid);
+	int rmax = max(a, mid + 1, r);
+	return lmax > rmax ? lmax : rmax;
+}
+
+void hanoi(int N, int d)
+{
+    if(N == 0)
+	{
+	    return;
+	}
+	hanoi( N- 1, -d );
+	std::cout<<"shift: "<<N<<"-"<<d<<std::endl;
+	hanoi( N- 1, -d );
+	
+	return;
+}
+
+int main(int arc, char **argv)
+{
+    int a[] = {9, 6, 7, 2, 4, 8, 1, 3, 5, 19};
+	std::cout<<max(a, 0, sizeof(a)/sizeof(int) - 1)<<std::endl;
+	hanoi(5, 1);
+}
