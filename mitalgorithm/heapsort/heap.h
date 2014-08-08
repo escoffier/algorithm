9db2f28e13718655f5e94ebb976e40951b01a98a
unknown <qunfengqiu@gmail.com>
2014-08-08 18:03:54 +0800

    add priority queue

--- mitalgorithm/heapsort/heap.h
+++ mitalgorithm/heapsort/heap.h
@@ -1,6 +1,7 @@
 #ifndef _HEAP_H_
 #define _HEAP_H_
-
+namespace qqf
+{
 template<class T>void exchange(T &a, T &b)
 {
     T temp = a;
@@ -45,4 +46,130 @@ template<class T> void heapsort(T a[], int size)
         maxheapify(a, --size, 0);
     }  
 };
+
+
+
+//heap_extract_max
+template<class T> void heappop(T a[], int& size)
+{
+    if(size < 1)
+    {
+        std::cout<<"no date to pop "<<std::endl;
+        return;
+    }
+    else if( 1== size)
+    {
+        size = 0;
+    }
+
+    exchange(a[0], a[size -1]);
+    maxheapify(a, size -1);
+}
+
+//HEAP_INCREASE_KEY
+template<class T> void heapshiftup(T a[], int size, int n, int key)
+{
+    if(size < 1 || n < 1 )
+    {
+        std::cout<<"size is 0"<<std::endl;
+        return;
+    }
+    a[n] = key;
+    while( n > 0)
+    {
+        if(a[n] <= a[(n+1)/2 -1])
+        {
+            break;
+        }
+        exchange(a[n], a[(n+1)/2 -1]);
+        n = (n+1)/2 -1;
+    }
+}
+
+template<class T> void heapinsert(T a[], int &size, int key)
+{
+    if(0 == size)
+    {
+        a[0] = key;
+    }
+    ++size;
+    a[size] = key;
+    heapshiftup(a, size, size - 1, key);
+}
+
+template<class T> class priority_queue
+{
+public:
+    priority_queue();
+    ~priority_queue();
+
+    void pop();
+    void push(const T &value);
+    const T& top() const;
+    size_t size() const;
+    bool empty() const;
+private:
+    T * buf_;
+    unsigned int size_;
+    unsigned int length_;
+};
+
+template<class T> priority_queue::priority_queue()
+{
+    length_ = 8
+    buf_ = new T[length_];
+    size_ = 0;
+}
+
+template<class T> priority_queue::~priority_queue()
+{
+    delete [] buf_;
+    buf_ = 0;
+    length_ = 0;
+    size_ = 0;
+}
+
+template<class T> void priority_queue::pop()
+{
+    if(size_ < 1)
+    {
+        std::cout<<"no date to pop "<<std::endl;
+        return;
+    }
+    else if( 1== size_)
+    {
+        size_ = 0;
+    }
+
+    exchange(buf_[0], a[size_ -1]);
+    maxheapify(buf_, size_ -1);
+}
+
+template<class T> void priority_queue::push(const T &value)
+{
+    if(0 == size_)
+    {
+        buf_[0] = value;
+    }
+    ++size_;
+    buf_[size_] = value;
+    heapshiftup(buf_, size_, size_ - 1, value);
+}
+
+template<class T> const T& priority_queue::top() const
+{
+    return buf_[0];
+}
+
+template<class T> size_t priority_queue::size() const
+{
+    return size_
+}
+
+template<class T> bool priority_queue::empty() const
+{
+    return ((!size_) ? false:true)
+}
+
+}
 #endif
--- mitalgorithm/heapsort/main.cpp
+++ mitalgorithm/heapsort/main.cpp
@@ -15,10 +15,16 @@ void printarray(int a[], int size)
 int main(int argc, char** argv)
 {
     int a[10] = {4,1,3,2,16,9,10,14,8,7};
-    buildheap(a, sizeof(a)/sizeof(int));
+    qqf::buildheap(a, sizeof(a)/sizeof(int));
     printarray(a, sizeof(a)/sizeof(int));
-    heapsort(a, sizeof(a)/sizeof(int));
+    qqf::heapsort(a, sizeof(a)/sizeof(int));
     printarray(a, sizeof(a)/sizeof(int));
+
+    int b = {15, 13, 9, 5, 12, 8, 7, 4, 0, 6, 2, 1};
+    qqf::buildheap(b, sizeof(b)/sizeof(int));
+    printarray(b, sizeof(b)/sizeof(int));
+    
+    qqf::priority_queue que<int>;
     return 0;
 }
 
